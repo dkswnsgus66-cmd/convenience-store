f03a432ade5044aae9357a9f20ed8f8d2206f4e6
unknown <dkswnsgus66@gmail.com>
2026-04-09 11:54:45 +0900

    s

--- src/main/java/com/tenco/dto/Sales.java
+++ src/main/java/com/tenco/dto/Sales.java
@@ -1,23 +1,22 @@
 package com.tenco.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+
+
 public class Sales {
 
-    private int id;                  // PK
-    private int productId;           // 판매된 상품 ID (FK)
-    private String productName;      // 조회 JOIN 결과 받을 때 사용 (매출 조회용)
-    private int quantity;            // 판매 수량
-    private BigDecimal unitPrice;    // 판매 당시 단가
-    private LocalDateTime soldAt;    // 판매 시각 (DATETIME → LocalDateTime)
+    private int productId;
+    private int quantity;
+    private double unitPrice;
+    private LocalDate soldAt;
+
 }
