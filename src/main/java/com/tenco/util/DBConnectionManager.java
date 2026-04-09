bbac14742a74117f531107be0822401918b7ff89
unknown <dkswnsgus66@gmail.com>
2026-04-09 11:56:32 +0900

    연결 업데이트

--- src/main/java/com/tenco/util/DBConnectionManager.java
+++ src/main/java/com/tenco/util/DBConnectionManager.java
@@ -1,9 +1,64 @@
 package com.tenco.util;
 
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+// DB 접근해서 Connection  객체를 생성하는 역할의 클래스 이다.
+// 싱글톤 패턴: 프로그램 전체에서 단 하나의 인스턴스만을 존재하게 하는 기법
 public class DBConnectionManager {
 
+    // static : 클래스 레벨에서 하나만 유지하는 커넥션 풀
+    private static final HikariDataSource dataSource;
+
+    // static 초기화 블록 : 클래스가 메모리에 로딩 될때 단 1번만 실행하는 녀석
+    static {
+
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/convenience_store");
+        config.setUsername(System.getenv("DB_USER"));
+        config.setPassword(System.getenv("DB_PASSWORD"));
+
+        config.setMaximumPoolSize(10); // 동시에 유지할 연결 최대 개수 갯수가 넘어가면 기다리게 된다 (동기화)
+        config.setMinimumIdle(3); // 확보할 최소 유후 커넥션 수 사람이 들어오지 않았을때 최소 3개는 연결해놓음
+        config.setConnectionTimeout(10000); // 커넥션 할당 대기시간 제한 (5초)
+        config.setIdleTimeout(600000); // 유후 커넥션 유지시간 (10분) 사람이 나가고 연결 유지 시간
+
+        dataSource = new HikariDataSource(config); // inputstrem의 bufer 와 비슷함
+
+    }
+    // 사용자가 호출하는 메서드 생성 : 풀에서 커넥션 객체 하나를 빌려온다
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    // 애플리케이션 종료시 커넥션 풀을 완전히 닫는다 (메모리 누수발생 때문)
+    public static void close(){
+
+        if(dataSource != null && !dataSource.isClosed()){
+            dataSource.close();
+        }
 
 
 
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            DBConnectionManager.getConnection();
+            Thread.sleep(100000);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
+
